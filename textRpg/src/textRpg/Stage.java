1dc95a3cc0174cb78544d5c85e60022ed977c738
kimseungwoo449 <ehdanf449@naver.com>
2024-04-04 17:52:56 +0900

    add : 부모 stage 생성자 , getStageName

--- textRpg/src/textRpg/Stage.java
+++ textRpg/src/textRpg/Stage.java
@@ -1,5 +1,15 @@
 package textRpg;
 
 abstract public class Stage {
+	private String stageName;
+	
+	public Stage(String stageName) {
+		this.stageName = stageName;
+	}
+	
 	abstract public void printMenu();
+	
+	public String getStageName() {
+		return this.stageName;
+	}
 }
