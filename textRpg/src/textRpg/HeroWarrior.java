49ea11856ac887312f0899b0fd26734ed7cedd93
kimseungwoo449 <ehdanf449@naver.com>
2024-04-04 10:47:02 +0900

    add : new Hero Warrior <- skill : next turn attack*3

--- textRpg/src/textRpg/HeroWarrior.java
+++ textRpg/src/textRpg/HeroWarrior.java
@@ -1,5 +1,28 @@
 package textRpg;
 
-public class HeroWarrior {
-
+public class HeroWarrior extends Hero {
+	private boolean isSkilled;
+	
+	
+	public HeroWarrior(int grade) {
+		super("전사", 1000, 65, grade);
+	}
+	
+	@Override
+	public int skill() {
+		if(isSkilled) {
+			return 0;
+		}		
+		this.isSkilled = true;
+		return 1;
+	}
+	
+	public boolean isSkilled() {
+		return this.isSkilled;
+	}
+	
+	public void setIsSkilled() {
+		this.isSkilled = this.isSkilled == false ? true : false;
+	}
+	
 }
