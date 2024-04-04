9ecaa61167e6334c3cee3ea10815948c9a78a12b
kimseungwoo449 <ehdanf449@naver.com>
2024-04-04 11:05:49 +0900

    add : new Monster <- skill : 자신의 공격력의 2배로 공격

--- textRpg/src/textRpg/MonsterOrc.java
+++ textRpg/src/textRpg/MonsterOrc.java
@@ -1,5 +1,15 @@
 package textRpg;
 
-public class MonsterOrc {
+public class MonsterOrc extends Monster{
+
+	public MonsterOrc(int maxHp, int offensivePower, int exp) {
+		super("오크", maxHp, offensivePower, exp);
+	}
+
+	@Override
+	public int skill(Unit target) {
+		int attack = this.getOffensivePower()*2;
+		return attack;
+	}
 
 }
