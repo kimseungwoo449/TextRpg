28f3a4d11253c5025e0e8725b74f01770bed1ae4
kimseungwoo449 <ehdanf449@naver.com>
2024-04-04 11:48:10 +0900

    add : 필드값, 생성자, initialHeros <- 초기엔 1등급 전사,마법사,성직자,성기사 줌 add : addHero, deleteHero

--- textRpg/src/textRpg/User.java
+++ textRpg/src/textRpg/User.java
@@ -1,5 +1,61 @@
 package textRpg;
 
-public class User {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+public class User {
+	private String id;
+	private String password;
+	
+	private int cash;
+	
+	private Map<Integer, ArrayList<Hero>> party;
+	private ArrayList<Hero> myHero;
+	private ArrayList<Item> inventory;
+	
+	public User(String id,String password) {
+		this.id = id;
+		this.password = password;
+		this.party= new HashMap<Integer, ArrayList<Hero>>();
+		this.myHero = initialHeros();
+		this.inventory = new ArrayList<Item>();
+		this.cash +=1000;
+	}
+	
+	private ArrayList<Hero> initialHeros(){
+		ArrayList<Hero> temp = new ArrayList<Hero>();
+		String[] className = {"HeroWarrior","HeroWizard","HeroPaladin","HeroPrist"};
+		Class<?>[] pram = new Class<?>[] {int.class};
+		
+		for(int i =0;i<className.length;i++) {
+			String path = "textRpg."+className[i];
+			try {
+				Class<?> clazz = Class.forName(path);
+				Object obj = clazz.getDeclaredConstructor(pram).newInstance(1);
+				Hero hero = (Hero)obj;
+				temp.add(hero);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return temp;
+	}
+	
+	public void addHero(Hero hero) {
+		this.myHero.add(hero);
+	}
+	
+	public boolean deleteHero(int index) {
+		if(index<0||index>=myHero.size())
+			return false;
+		
+		this.myHero.remove(index);
+		return true;
+	}
+	
+	public void showMyHero() {
+		
+	}
+	
 }
