c3391b4f2e3e8de954a15f8d3ebb29031f256c65
kimseungwoo449 <ehdanf449@naver.com>
2024-04-04 11:08:14 +0900

    add : new Monster Wolf <- skill : 자신의 공격력의 1.5배로 공격

--- textRpg/src/textRpg/MonsterWolf.java
+++ textRpg/src/textRpg/MonsterWolf.java
@@ -1,5 +1,14 @@
 package textRpg;
 
-public class MonsterWolf {
+public class MonsterWolf extends Monster{
+	
+	public MonsterWolf( int maxHp, int offensivePower, int exp) {
+		super("늑대", maxHp, offensivePower, exp);
+	}
 
+	@Override
+	public int skill(Unit target) {
+		int attack = (int)(this.getOffensivePower()*1.5);
+		return attack;
+	}
 }
