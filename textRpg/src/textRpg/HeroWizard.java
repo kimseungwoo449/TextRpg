6f7f0597e26d76ed5588913d6263c3bf0f98a61d
kimseungwoo449 <ehdanf449@naver.com>
2024-04-04 10:50:33 +0900

    add : new Hero Wizard -> skill : fireball 내 공격력의 1.5배로 공격

--- textRpg/src/textRpg/HeroWizard.java
+++ textRpg/src/textRpg/HeroWizard.java
@@ -1,5 +1,16 @@
 package textRpg;
 
-public class HeroWizard {
+public class HeroWizard extends Hero{
+
+	public HeroWizard( int grade) {
+		super("마법사", 600, 50, grade);
+	}
+
+	@Override
+	public int skill(Unit target) {
+		int myOffensivePower = super.getOffensivePower();
+		int attack = myOffensivePower + (int) (myOffensivePower * 1.5);
+		return attack;
+	}
 
 }
