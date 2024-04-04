0b797df26f1991bc81a6745ccfbd495fe73a7576
kimseungwoo449 <ehdanf449@naver.com>
2024-04-04 12:06:31 +0900

    modify : toString Hero

--- textRpg/src/textRpg/Hero.java
+++ textRpg/src/textRpg/Hero.java
@@ -1,21 +1,21 @@
 package textRpg;
 
-abstract public class Hero extends Unit{
+abstract public class Hero extends Unit {
 	private int grade;
 	private int lv;
-	
+
 	public Hero(String name, int maxHp, int offensivePower, int grade) {
-		super(name, maxHp, offensivePower, 0);	// 이름, 최대 체력, 공격력, 경험치(0) 시작
+		super(name, maxHp, offensivePower, 0); // 이름, 최대 체력, 공격력, 경험치(0) 시작
 		this.grade = grade;
 		super.setMaxHp(grade);
 		super.setOffensivePower(grade);
 		this.lv = 1;
 	}
-	
+
 	public void reciveExp(int exp) {
 		super.setExp(exp);
 	}
-	
+
 	public void checkLevelUp() {
 		if (super.getExp() >= 100) {
 			this.lv += 1;
@@ -23,4 +23,10 @@ abstract public class Hero extends Unit{
 		}
 	}
 
+	@Override
+	public String toString() {
+		String info = String.format("[%s][Lv : %d]", this.grade == 1 ? "★" : this.grade == 2 ? "★★" : "★★★");
+		info+=this;
+		return info;
+	}
 }
