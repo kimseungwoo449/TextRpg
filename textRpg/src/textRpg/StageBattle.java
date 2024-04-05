b6af275d13aa5faf26fc212d45743bab7a224548
kimseungwoo449 <ehdanf449@naver.com>
2024-04-05 14:09:41 +0900

    modify : 몬스터 또는 영울이 죽으면 출력 추가

--- textRpg/src/textRpg/StageBattle.java
+++ textRpg/src/textRpg/StageBattle.java
@@ -12,9 +12,9 @@ public class StageBattle extends Stage {
 
 	private ArrayList<Unit> party;
 	private ArrayList<Unit> monsters;
-	
+
 	private UserManager userManager = UserManager.getInstance();
-	
+
 	public StageBattle(ArrayList<Unit> party, ArrayList<Unit> monsters) {
 		super("BATTLE");
 		this.party = party;
@@ -79,7 +79,7 @@ public class StageBattle extends Stage {
 		return true;
 	}
 
-	private void selectNextAction() {
+	private String selectNextAction() {
 		String heroesAction = "";
 		for (int i = 0; i < party.size(); i++) {
 			Unit target = party.get(i);
@@ -92,17 +92,17 @@ public class StageBattle extends Stage {
 			info += String.format("[%s] [1] 어택 [2] 스킬 [3] 아이템", hero.getName());
 			Color.greenPrintln(info);
 
-			String curHeroAction = runMenu(hero);
+			String curHeroAction = actionOfHero(hero);
 			if (curHeroAction == null) {
 				i--;
 				continue;
 			}
-			heroesAction += curHeroAction+"\n";
+			heroesAction += curHeroAction + "\n";
 		}
-		Color.whitePrint(heroesAction);
+		return heroesAction;
 	}
 
-	private String runMenu(Hero hero) {
+	private String actionOfHero(Hero hero) {
 		int choice = GameManager.inputNumber("Menu");
 		if (choice == ATTACK)
 			return attack(hero);
@@ -124,7 +124,10 @@ public class StageBattle extends Stage {
 		}
 
 		hero.attack(monster, attack);
-		String info = String.format("[%s]가 [%s]를 %d만큼 공격했습니다.!", hero.getName(), monster.getName(), attack);
+		String info = String.format("[%s]가 [%s]를 %d만큼 공격했습니다!", hero.getName(), monster.getName(), attack);
+		if (monster.isDead()) {
+			info += String.format("\n[%s]가 죽었습니다!", monster.getName());
+		}
 		return info;
 	}
 
@@ -135,20 +138,25 @@ public class StageBattle extends Stage {
 			int attack = hero.skill(monster);
 			hero.attack(monster, attack);
 			info += String.format("[파이어볼]!! [%s]가 [%s]를 %d만큼 공격했습니다!", hero.getName(), monster.getName(), attack);
+			
+			if (monster.isDead()) {
+				info += String.format("\n[%s]가 죽었습니다!", monster.getName());
+			}
+			
 		} else if (hero instanceof HeroPaladin) {
 			int heal = hero.skill(hero);
-			info+=String.format("[%s]가 %d만큼 회복했습니다.", hero.getName(),heal);
-		}else if(hero instanceof HeroWarrior) {
+			info += String.format("[%s]가 %d만큼 회복했습니다.", hero.getName(), heal);
+		} else if (hero instanceof HeroWarrior) {
 			int setSkilled = hero.skill(hero);
-			if(setSkilled==0) {
+			if (setSkilled == 0) {
 				Color.redPrintln("이미 강화된 상태입니다.");
 				return null;
 			}
-			info+=String.format("[%s]가 공격을 강화했습니다. 다음 턴엔 공격력의 3배로 공격합니다.", hero.getName());
-		}else if(hero instanceof HeroPrist) {
-			Hero target = (Hero)choiceRandomUnit(party);
+			info += String.format("[%s]가 공격을 강화했습니다. 다음 턴엔 공격력의 3배로 공격합니다.", hero.getName());
+		} else if (hero instanceof HeroPrist) {
+			Hero target = (Hero) choiceRandomUnit(party);
 			int heal = hero.skill(target);
-			info+=String.format("[%s]가 [%s]를 %d만큼 회복시켰습니다!", hero.getName(),target.getName(),heal);
+			info += String.format("[%s]가 [%s]를 %d만큼 회복시켰습니다!", hero.getName(), target.getName(), heal);
 		}
 
 		return info;
@@ -157,7 +165,7 @@ public class StageBattle extends Stage {
 	private Unit choiceRandomUnit(ArrayList<Unit> group) {
 		Unit target = null;
 		while (true) {
-			int randomUnit = ran.nextInt(monsters.size());
+			int randomUnit = ran.nextInt(group.size());
 			target = group.get(randomUnit);
 
 			if (!target.isDead())
@@ -165,33 +173,92 @@ public class StageBattle extends Stage {
 		}
 		return target;
 	}
-	
+
 	private String useItem(Hero hero) {
 		userManager.showMyInventory();
-		int itemIndex = GameManager.inputNumber("사용할 아이템 번호")-1;
-		
+		int itemIndex = GameManager.inputNumber("사용할 아이템 번호") - 1;
+
 		Item item = userManager.getItem(itemIndex);
-		
-		if(item instanceof ItemBomb) {
-			ItemBomb bomb = (ItemBomb)item;
+		String info = null;
+		if (item instanceof ItemBomb) {
+			ItemBomb bomb = (ItemBomb) item;
 			Unit monster = choiceRandomUnit(monsters);
 			bomb.fucntion(monster);
-			return String.format("[%s]가 [%s]를 사용해 [%s]를 %d의 데미지로 공격합니다.", hero.getName(),bomb.getName(),monster.getName(),bomb.getDamage());
-		}else if(item instanceof ItemPotion) {
-			ItemPotion potion = (ItemPotion)item;
-			potion.fucntion(hero);
-			return String.format("[%s]가 [%s]를 사용해 체력 %d를 회복합니다.", hero.getName(),potion.getName(),potion.getRecoveryAmount());
-		}
+			info = String.format("[%s]가 [%s]를 사용해 [%s]를 %d의 데미지로 공격합니다.", hero.getName(), bomb.getName(),
+					monster.getName(), bomb.getDamage());
 			
-		return null;
+			if (monster.isDead()) {
+				info += String.format("\n[%s]가 죽었습니다!", monster.getName());
+			}
+		} else if (item instanceof ItemPotion) {
+			ItemPotion potion = (ItemPotion) item;
+			potion.fucntion(hero);
+			info = String.format("[%s]가 [%s]를 사용해 체력 %d를 회복합니다.", hero.getName(), potion.getName(),
+					potion.getRecoveryAmount());
+		}
+
+		return info;
 	}
-	
-	
+
+	private String attackOfMonsters() {
+		String monstersAction = "";
+		for (Unit monster : monsters) {
+			int randomHero = ran.nextInt(party.size());
+			Unit hero = party.get(randomHero);
+			if (!monster.isDead() && !hero.isDead()) {
+				int randomSkill = ran.nextInt(4);
+				int attack = 0;
+
+				if (randomSkill == 0) {
+					attack = monster.skill(hero);
+				} else {
+					attack = monster.getOffensivePower();
+				}
+
+				monstersAction += makeText(monster, hero, attack, randomSkill) + "\n";
+				if (monster instanceof MonsterSlime && randomSkill == 0) {
+					continue;
+				}
+				hero.takeDamage(attack);
+				if (hero.isDead()) {
+					monstersAction += String.format("[%s]에 의해 [%s]가 사망했습니다....\n", monster.getName(), hero.getName());
+				}
+			}
+		}
+		return monstersAction;
+	}
+
+	private String makeText(Unit monster, Unit hero, int value, int skilled) {
+		String info = null;
+		if (skilled == 0) {
+			if (monster instanceof MonsterSlime) {
+				info = String.format("[자가재생][%s]가 %s만큼 자가회복을 했습니다.", monster.getName(),value);
+			} else if (monster instanceof MonsterBat) {
+				info = String.format("[흡혈][%s]가 [%s]에게 %d만큼의 피해를 준 뒤, %d만큼 회복합니다.", monster.getName(), hero.getName(),
+						value, value * 2);
+			} else if (monster instanceof MonsterOrc) {
+				info = String.format("[강타][%s]가 [%s]에게 %d만큼의 피해를 주었습니다.", monster.getName(), hero.getName(), value);
+			} else if (monster instanceof MonsterWolf) {
+				info = String.format("[물어뜯기][%s]가 [%s]에게 %d만큼의 피해를 주었습니다.", monster.getName(), hero.getName(), value);
+			}
+		} else {
+			info = String.format("[%s]가 [%s]에게 %d만큼의 피해를 주었습니다.", monster.getName(), hero.getName(), value);
+		}
+		return info;
+	}
+
+	private void printTurnStatus(String heroesAction, String monstersAction) {
+		Color.greenPrintln("----- Turn Status -----");
+		Color.whitePrint(heroesAction);
+		Color.redPrint(monstersAction);
+	}
+
 	public void run() {
 		while (checkNext()) {
 			printMenu();
-			selectNextAction();
-			
+			String heroesAction = selectNextAction();
+			String monstersAction = attackOfMonsters();
+			printTurnStatus(heroesAction, monstersAction);
 		}
 	}
 
